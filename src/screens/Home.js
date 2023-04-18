c50fbe3d5d14c4d6253a515ee942d05158045f0a
YOUR NAME <banerjeesagnik29@gmail.com>
2023-04-18 14:13:19 +0530

    options

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,12 +6,21 @@ export default function Home() {
     <div>
       <div>
         <Navbar />
-        <div></div>
-        <div className="card" style={{"width": "18rem","fontFamily":"fantasy"}}>
+       
+
+        <div className="card" style={{"width": "18rem","maxHeight":"360px"}}>
           <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p className="card-text">Lorem Ipsum</p>
+              <div className='container w-500 bg-success'>
+              <select className="m-2 h-100 w-20 text-black rounded" style={{ select: "#0055FF"}}>
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>{i + 1}</option>)
+              })}
+            </select>
+              </div>
               
             </div>
         </div>
