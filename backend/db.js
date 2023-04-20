e7d769fac989ad233088e0b6fcbfa71fc0a67b11
YOUR NAME <banerjeesagnik29@gmail.com>
2023-04-20 13:42:27 +0530

    API call

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
-const mongoURI='mongodb+srv://banerjeesagnik29:Arju01%40%25@cluster0.nh3sdco.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI='mongodb+srv://banerjeesagnik29:Arju01%40%25@cluster0.nh3sdco.mongodb.net/Foodie?retryWrites=true&w=majority';
 
 const mongoDB=async()=>{
-await mongoose.connect(mongoURI,{useNewURlParser:true},(err,result)=>{
+await mongoose.connect(mongoURI,{useNewURlParser:true},async(err,result)=>{
 
     if(err)
     console.log("---",err);
     else
+    {
     console.log("connected");
+    const fetched_data=await mongoose.connection.db.collection("food_items");
+    fetched_data.find({}).toArray(function(err,data)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            console.log();
+        }
+    })
+    
+    }
 });
 }
 
